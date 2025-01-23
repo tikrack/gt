102819b786e74aa0358149893b84851f68c0a304
mohamadreza <mohamadreza1388.org@gmail.com>
2025-01-23 12:55:31 +0330

    finish

--- gt.bat
+++ gt.bat
@@ -2,20 +2,14 @@
 setlocal
 
 if "%~1"=="" (
-    echo Usage: gt "commit message"
+    echo Message is empty
     exit /b
 )
 
 set COMMIT_MSG=%~1
 
-echo Running git commands...
-echo [git add .]
 git add .
-
-echo [git commit -m "%COMMIT_MSG%"]
 git commit -m "%COMMIT_MSG%"
-
-echo [git push]
 git push
 
 endlocal
